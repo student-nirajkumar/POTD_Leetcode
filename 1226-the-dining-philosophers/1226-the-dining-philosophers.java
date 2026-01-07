9bec63ac45c6973bd2ff22661a31014e24b79985
Niraj Kumar <166704991+student-nirajkumar@users.noreply.github.com>
2026-01-07 17:32:56 +0530

    Time: 11 ms (56.58%), Space: 53.3 MB (24.47%) - LeetHub

---
+++ 1226-the-dining-philosophers/1226-the-dining-philosophers.java
@@ -0,0 +1,49 @@
+
+
+public class DiningPhilosophers {
+
+    private Lock leftForkLock = new ReentrantLock();
+    private Lock rightForkLock = new ReentrantLock();
+
+    public DiningPhilosophers() {
+
+    }
+
+    // call the run() method of any runnable to execute its code
+    public void wantsToEat(int philosopher,
+                           Runnable pickLeftFork,
+                           Runnable pickRightFork,
+                           Runnable eat,
+                           Runnable putLeftFork,
+                           Runnable putRightFork) throws InterruptedException {
+
+
+        while(true) {
+            if(leftForkLock.tryLock(100, TimeUnit.MILLISECONDS)) {
+                try {
+                    pickLeftFork.run();
+
+                    if (rightForkLock.tryLock(100, TimeUnit.MILLISECONDS)) {
+                        try {
+                            pickRightFork.run();
+                            eat.run();
+                            putRightFork.run();
+
+                            /*
+                             * we have eaten
+                             */
+                            return;
+                        } finally {
+                            rightForkLock.unlock();
+                        }
+                    }
+
+
+                } finally {
+                    putLeftFork.run();
+                    leftForkLock.unlock();
+                }
+            }
+        }
+    }
+}
