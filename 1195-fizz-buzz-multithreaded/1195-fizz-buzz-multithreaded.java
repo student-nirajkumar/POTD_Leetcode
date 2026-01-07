3b8b1cea5f2f12682a62d06210f02dede1374f01
Niraj Kumar <166704991+student-nirajkumar@users.noreply.github.com>
2026-01-07 16:44:13 +0530

    Time: 5 ms (70.33%), Space: 50.1 MB (39.29%) - LeetHub

---
+++ 1195-fizz-buzz-multithreaded/1195-fizz-buzz-multithreaded.java
@@ -0,0 +1,57 @@
+class FizzBuzz {
+
+    private int n;
+    private int i = 1;   
+
+    public FizzBuzz(int n) {
+        this.n = n;
+    }
+    public synchronized void fizz(Runnable printFizz) throws InterruptedException {
+        while (i <= n) {
+            while (i <= n && !(i % 3 == 0 && i % 5 != 0)) {
+                wait();
+            }
+            if (i <= n) {
+                printFizz.run();
+                i++;
+            }
+            notifyAll();
+        }
+    }
+    public synchronized void buzz(Runnable printBuzz) throws InterruptedException {
+        while (i <= n) {
+            while (i <= n && !(i % 5 == 0 && i % 3 != 0)) {
+                wait();
+            }
+            if (i <= n) {
+                printBuzz.run();
+                i++;
+            }
+            notifyAll();
+        }
+    }
+    public synchronized void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
+        while (i <= n) {
+            while (i <= n && !(i % 3 == 0 && i % 5 == 0)) {
+                wait();
+            }
+            if (i <= n) {
+                printFizzBuzz.run();
+                i++;
+            }
+            notifyAll();
+        }
+    }
+    public synchronized void number(IntConsumer printNumber) throws InterruptedException {
+        while (i <= n) {
+            while (i <= n && (i % 3 == 0 || i % 5 == 0)) {
+                wait();
+            }
+            if (i <= n) {
+                printNumber.accept(i);
+                i++;
+            }
+            notifyAll();
+        }
+    }
+}
