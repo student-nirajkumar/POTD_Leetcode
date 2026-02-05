8f73f39976509ed70c7a9911a7b3f09677fc2456
Niraj Kumar <166704991+student-nirajkumar@users.noreply.github.com>
2026-02-05 13:09:03 +0530

    Time: 0 ms (100%), Space: 43 MB (9.91%) - LeetHub

--- 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.java
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.java
@@ -1,29 +1,15 @@
 class Solution {
     public int maxChunksToSorted(int[] arr) {
+        int cummSum = 0;
+        int sum = 0;
         int n = arr.length;
-        int prefix[] = new int[n];
-        int suffix[] = new int[n];
-        prefix[0] = arr[0];
-        suffix[n - 1] = arr[n - 1];
-        for (int i = 1; i < n; i++) {
-            prefix[i] = Math.max(arr[i], prefix[i - 1]);
-        }
-        for (int i = n - 2; i >= 0; i--) {
-            suffix[i] = Math.min(arr[i], suffix[i + 1]);
-        }
-        int pahleKaMax = 0;
-        int badKaMin = 0;
         int chunk = 0;
-        for (int i = 0; i < n; i++) {
-            if (i - 1 < 0) {
-                pahleKaMax = -1;
-            } else {
-                pahleKaMax =prefix[i - 1];
-            }
-            badKaMin = suffix[i];
-            if (pahleKaMax < badKaMin) {
+        for(int i=0; i<n; i++){
+             cummSum+=arr[i];
+             sum += i;
+             if(cummSum == sum){
                 chunk++;
-            }
+             }
         }
         return chunk;
     }
