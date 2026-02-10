bd0e02bbfc60ce68e4d1fdef010ff58f92c7064c
Niraj Kumar <166704991+student-nirajkumar@users.noreply.github.com>
2026-02-10 14:37:46 +0530

    Time: 305 ms (64.42%), Space: 47.4 MB (11.31%) - LeetHub

---
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int longestBalanced(int[] nums) {
+        int n = nums.length;
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            HashSet<Integer> evenSet = new HashSet<>();
+            HashSet<Integer> oddSet = new HashSet<>();
+
+            for (int j = i; j < n; j++) {
+                if (nums[j] % 2 == 0) {
+                    evenSet.add(nums[j]);
+                } else {
+                    oddSet.add(nums[j]);
+                }
+
+                if (evenSet.size() == oddSet.size()) {
+                    max = Math.max(max, j - i + 1);
+                }
+            }
+        }
+        return max;
+    }
+}
