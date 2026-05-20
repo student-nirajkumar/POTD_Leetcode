018eaed1ff87d0558dd540f7167070707c3f098d
Niraj Kumar <166704991+student-nirajkumar@users.noreply.github.com>
2026-05-20 16:26:51 +0530

    Time: 2 ms (72.65%), Space: 44.8 MB (34.03%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,57 +1,41 @@
 class Solution {
 
-    public boolean isValid(String str){
+    public void generate(String curr, int n, int length,
+                         int open, int close, List<String> res) {
 
-        int sum = 0;
+        if (length == 2 * n) {
 
-        for(int i = 0; i < str.length(); i++){
-
-            char ch = str.charAt(i);
-
-            if(ch == '('){
-                sum++;
-            }
-            else{
-                sum--;
-            }
-
-            if(sum < 0){
-                return false;
-            }
-        }
-
-        return sum == 0;
-    }
-
-    public void generate(String curr, int n, int length, List<String> res){
-
-        if(length == 2 * n){
-
-            if(isValid(curr)){
-                res.add(curr);
-            }
-
+            res.add(curr);
             return;
         }
 
-        curr += '(';
+        if (open < n) {
 
-        generate(curr, n, length + 1, res);
+            curr += '(';
 
-        curr = curr.substring(0, curr.length() - 1);
+            generate(curr, n, length + 1, open + 1, close, res);
 
-        curr += ')';
+            curr = curr.substring(0, curr.length() - 1);
+        }
 
-        generate(curr, n, length + 1, res);
+        if (close < open) {
 
-        curr = curr.substring(0, curr.length() - 1);
+            curr += ')';
+
+            generate(curr, n, length + 1, open, close + 1, res);
+
+            curr = curr.substring(0, curr.length() - 1);
+        }
     }
 
     public List<String> generateParenthesis(int n) {
 
         List<String> res = new ArrayList<>();
 
-        generate("", n, 0, res);
+        int open = 0;
+        int close = 0;
+
+        generate("", n, 0, open, close, res);
 
         return res;
     }
